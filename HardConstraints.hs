6b73f1682ca9068ba7f37a7d92d812da80368ff2
JZuCuadra <jesusandresck@gmail.com>
2018-03-18 11:29:51 -0600

    Add files via upload
    Added a few changes to tntCheck to account for edge case of new task being first of the tuple and the last one, for the case where tnt is of the form (task in machine 8, task in machine 1)

--- HardConstraints.hs
+++ HardConstraints.hs
@@ -9,21 +9,26 @@
 fpaCheck	::	[Char] -> Bool															-- Setup Function I/O
 fpaCheck 	Assignment		= 	index :: (length Assignment - 1)						-- Get the index/depth of current node
 								task :: Assignment !! index								-- Retrieve associated task
-								return (my_element `(length Assignment, task)` fpaList)	-- Search for a tuple match of form (Int, Char) withing the fpaList and return
+								return (my_element `(length Assignment, task)` fpaList)	-- Search for a tuple match of form (Int, Char) within the fpaList and return
 ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------								
 fmCheck		::	[Char] -> Bool															-- Setup Function I/O
 fmCheck		Assignment		=	index :: (length Assignment - 1)						-- Get the index/depth of current node
 								task :: Assignment !! index								-- Retrieve associated task
-								return (my_element `(length Assignment, task)` fmList)	-- Search for a tuple match of form (Int, Char) withing the fmList and return
+								return (my_element `(length Assignment, task)` fmList)	-- Search for a tuple match of form (Int, Char) within the fmList and return
 ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------												
 tntCheck	::	[Char] -> Bool															-- Setup Function I/O
 tntCheck	Assignment		=	index :: (length Assignment - 1)						-- Get the index/depth of current node
 							if index = 0												-- Check to see if index is 0
 								return False											-- If so, ignore check and return False
-								else do prevIndex :: (index - 1)						-- Else, get the previous index/depth of the node
-								task :: Assignment !! index								-- Retrieve associated task
-								prevTask :: Assignment !! prevIndex						-- Retrieve previous associated task
-								return (my_element `(prevTask, task)` tntList)			-- Search for a tuple match of form (Int, Int) withing the tntList and return
+								else if index = 7										-- Check to see if index is 0
+									task :: Assignment !! index							-- If so, retrieve associated task
+									nextTask :: Assignment !! 0							-- Retrieve associated task at index 0
+									if (my_element `(task, nextTask)` tntList)			-- Search for a tuple match of form (Int, Int) within the tntList
+										return True										-- If match found, return True
+										else do prevIndex :: (index - 1)				-- Else, get the previous index/depth of the node
+										task :: Assignment !! index						-- Retrieve associated task
+										prevTask :: Assignment !! prevIndex				-- Retrieve previous associated task
+										return (my_element `(prevTask, task)` tntList)	-- Search for a tuple match of form (Int, Int) within the tntList and return
 ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------								
 chedkHC		::	[Char] -> Bool															-- Setup Function I/O
 checkHC 	Assignment		= 	do fpaCheck Assignment									-- Call fpaCheck
