e5a342aa18266efcc109acaf870cb711cb7261c6
thdong <tony.hao.dong@gmail.com>
2018-03-18 17:10:18 -0600

    Add files via upload

--- HardConstraints.hs
+++ HardConstraints.hs
@@ -1,6 +1,6 @@
 {-
 	CPSC 449
-	Assignment 2
+	assignment 2
 	Hard Constraints
 	Jesus Cuadra
 	Version 1.03
@@ -8,29 +8,34 @@
 module HardConstraints
 ( checkHC
 ) where
+
+
 ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-fpaCheck	::	[Char] -> Bool																							-- Setup Function I/O
-fpaCheck 	Assignment		=	let index = (length Assignment - 1)														-- Get the index/depth of current node
-								in return (my_element (length Assignment, last Assignment) fmList)						-- Search for a tuple match of form (Int, Char) within the fmList and return
+fpaCheck	::	String -> Bool
+fpaCheck [] = False																						-- Setup Function I/O
+fpaCheck 	assignment		=	let index = (length assignment - 1)														-- Get the index/depth of current node
+								in ((length assignment, last assignment) `elem` fmList)						-- Search for a tuple match of form (Int, Char) within the fmList and return
 ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------								
-fmCheck		::	[Char] -> Bool																							-- Setup Function I/O
-fmCheck		Assignment		=	let index :: (length Assignment - 1)													-- Get the index/depth of current node
-								in return (my_element (length Assignment, last Assignment) fmList)						-- Search for a tuple match of form (Int, Char) within the fmList and return
+fmCheck		::	String -> Bool
+fmCheck []	= False 																							-- Setup Function I/O
+fmCheck		assignment		=	let index = (length assignment - 1)													-- Get the index/depth of current node
+								in ((length assignment, last assignment) `elem` fmList)						-- Search for a tuple match of form (Int, Char) within the fmList and return
 ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------												
-tntCheck	::	[Char] -> Bool																							-- Setup Function I/O
-tntCheck	Assignment		=	let index :: (length Assignment - 1)													-- Get the index/depth of current node.
+tntCheck	::	String -> Bool	
+tntCheck [] = False																						-- Setup Function I/O
+tntCheck	assignment		=	let index = (length assignment - 1)													-- Get the index/depth of current node.
 								in if index == 0																		-- Check to see if index is 0.
 									then False																			-- If so return False
 									else if index == 7																	-- Check to see if index is 7.
-										then (my_element ((Assignment !! index), (Assignment !! 0)) tntList) && (my_element ((Assignment !! (index - 1)), (Assignment !! index)) tntList)			
+										then (((assignment !! index), (assignment !! 0)) `elem` tntList) && (((assignment !! (index - 1)), (assignment !! index)) `elem` tntList)			
 										-- If so attempt to find the edge case tuple, and then attempt to find find the tuple of (index - 1, index)
-										else (my_element ((Assignment !! (index - 1)), (Assignment !! index)) tntList)	-- Attempt to find find the tuple of (index - 1, index)
+										else (((assignment !! (index - 1)), (assignment !! index)) `elem` tntList)	-- Attempt to find find the tuple of (index - 1, index)
 ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------								
-checkHC		::	[Char] -> Bool																							-- Setup Function I/O
-checkHC 	Assignment		= 	if fpaCheck Assignment == False															-- Verify fpaCheck's return
+checkHC		::	String -> Bool																							-- Setup Function I/O
+checkHC 	assignment		= 	if fpaCheck assignment == False															-- Verify fpaCheck's return
 									then False																			-- If False, return False
-									else if fmCheck Assignment == True													-- Else verify fmCheck's return
+									else if fmCheck assignment == True													-- Else verify fmCheck's return
 											then False																	-- If True, return False
-											else if tntCheck Assignment == True											-- Else Verify tntCheck's return
+											else if tntCheck assignment == True											-- Else Verify tntCheck's return
 												then False																-- If True, return False
-												else True																-- Else return True
+												else True																-- Else return t
